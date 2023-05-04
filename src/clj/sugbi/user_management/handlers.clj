f45743a6c2b11886544a1e4578744ebf0dbb6fd8
albert <natanael_pi@ciencias.unam.mx>
2023-05-04 12:50:18 -0600

    Added some user.handler logic.

--- src/clj/sugbi/user_management/handlers.clj
+++ src/clj/sugbi/user_management/handlers.clj
@@ -1,5 +1,11 @@
-(ns sugbi.user-management.handlers)
+(ns sugbi.user-management.handlers
+  (:require
+   [ring.util.http-response :as response]))
 
 (defn get-all-lendings
   [request]
+  (if-let [librarian (get-in request [:session :is-librarian?])]
+    (if-let [query (get-in request [:parameters :query :q])]
+      (response/ok
+       ())))
   )
--- src/clj/sugbi/user_management/routes.clj
+++ src/clj/sugbi/user_management/routes.clj
@@ -1,17 +1,28 @@
 (ns sugbi.user-management.routes
   (:require
    [spec-tools.data-spec :as ds]
-   [sugbi.user-management.handlers :as users.handlers]))
+   [sugbi.user-management.handlers :as users.handlers])
+   (:import java.io.BufferedInputStream
+           java.time.LocalDate))
+
+(defn date?
+  [o]
+  (instance? java.time.LocalDate o))
 
 (def book-loan-info-spec
   {:book-lending-id int?
    :book-id int?
-   :user_id int?})
+   :user-id int?
+   :loan-init-date date?
+   :loan-due-date date?})
 
 (def routes
   ["/user" {:swagger {:tags ["Users"]}}
-   ["/lendings" {:get {:summary "returns all loans of the user with the specified id"
+   ["/lendings" {:get {:summary "returns all loans of the user with the specified id.
+                                 Optionally, can take a query but only for librarians"
                        :parameters {:query {(ds/opt :q) string?}
                                     :header {:cookie string?}}
-                       :responses {200 {:body [book-loan-info-spec]}}
+                       :responses {200 {:body [book-loan-info-spec]}
+                                   404 {:body {:user-id int?}}
+                                   403 {:body {:message string?}}}
                        :handler users.handlers/get-all-lendings}}]])
